5f275bd1ff5c23f63c320ed337fd826f116e2ea7
sbraconnier <simonbraconnier@gmail.com>
2020-03-15 13:04:54 -0400

    Upgrade to jodconverter 4.3.1-SNAPSHOT

--- src/main/java/org/jodconverter/nabeel/BootApplication.java
+++ src/main/java/org/jodconverter/nabeel/BootApplication.java
@@ -10,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.util.ResourceUtils;
 
-import org.jodconverter.DocumentConverter;
+import org.jodconverter.core.DocumentConverter;
 
 /**
  * Main application.
